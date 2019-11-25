b4a471ccc763e27634decc0f526160060fa73744
Patryk Cieszyński <patrykciesz@gmail.com>
2019-11-25 11:56:06 +0100

    2 exercises from PracticePython.org

---
+++ PracticePython.org/ex1/Source.py
@@ -0,0 +1,6 @@
+import datetime
+date=datetime.datetime.now()
+name=input("Enter name: ")
+age=int(input("Enter your age: "))
+multiply=int(input("Enter number for multiplication: "))
+print(multiply*(name + " will turn 100 in " + str(date.year-age+100) + "\n"))
